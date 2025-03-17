464b5720043ec4d8e0c99c41639bc0567cfcbba9
lijovijayan <lijovijayan00@gmail.com>
2025-03-17 16:05:39 +0530

    build failure fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -567,8 +567,8 @@
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
 #define TEMP_SENSOR_0 1
-#define TEMP_SENSOR_1 0
-#define TEMP_SENSOR_2 0
+#define TEMP_SENSOR_1 1
+#define TEMP_SENSOR_2 1
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_5 0
