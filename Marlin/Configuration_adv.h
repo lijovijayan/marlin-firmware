cb953d7458b35068746dc38267a1840d1a23751b
lijovijayan <lijovijayan00@gmail.com>
2025-04-26 00:24:51 +0530

    updated default current for y axis to reduce heating

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3015,7 +3015,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Y)
-    #define Y_CURRENT       300
+    #define Y_CURRENT       250
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
